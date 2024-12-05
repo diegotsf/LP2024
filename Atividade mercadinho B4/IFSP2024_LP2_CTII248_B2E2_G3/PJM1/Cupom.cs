577ca4ea1da5255a469e08bed37872c4f14ce96b
diegotsf <126783973+diegotsf@users.noreply.github.com>
2024-12-05 20:30:46 -0300

    Add files via upload

---
+++ Atividade mercadinho B4/IFSP2024_LP2_CTII248_B2E2_G3/PJM1/Cupom.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJM1
+{ //Feito por João Gabriel F. Souza, Ricardo Amaro e Diego Toscano.
+    internal class Cupom
+    {
+        public double Total { get; private set; } = 0;
+        private int _numeroCupom;
+        private DateTime _dataEmissaoCupom;
+        private string _cpfCliente;
+        private List<ItemCupom> _itemCupom = new List<ItemCupom>();
+        int i = 0;
+        public List<ItemCupom> GetItemCupom()
+        {
+            return this._itemCupom;
+        }
+
+
+        public void setNumeroCupom(int e)
+        {
+            this._numeroCupom = e;
+        }
+        public DateTime GetDataEmissao()
+        {
+
+            return DateTime.Now;
+        }
+
+        public void setCpfCliente(string e)
+        {
+            this._cpfCliente = e;
+        }
+
+        public void setItemCupom(ItemCupom item)
+        {
+            if (_itemCupom.Count < 20)
+            {
+                this._itemCupom.Add(item);
+                Total += item.Valor;
+            }
+            else
+            {
+                Console.WriteLine("Não é possível adicionar mais itens ao cupom. Capacidade máxima atingida.");
+            }
+        }
+
+    }
+    internal class ItemCupom
+    {
+        public Produtos Produto { get; set; }
+        public float Quantidade { get; set; }
+        public double Valor { get; set; }
+
+
+        public ItemCupom()
+        {
+        }
+        public ItemCupom(Produtos produto, float quantidade)
+        {
+            Produto = produto;
+            Quantidade = quantidade;
+            Valor = produto.Preco * quantidade;
+        }
+    }
+}
+
---
+++ Atividade mercadinho B4/IFSP2024_LP2_CTII248_B2E2_G3/PJM1/Produtos.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJM1
+{ //Feito por João Gabriel F. Souza, Ricardo Amaro e Diego Toscano.
+    internal class Produtos
+    {
+        public string Descricao;
+        public double Preco;
+        public string Codigo;
+        public string Unidade;
+        public int Quantidade;
+        public double Valor
+        {
+            get { return Preco * Quantidade; }
+        }
+
+
+        internal Produtos(string cod, string desc, double pre, int quan, string uni)
+        {
+            Codigo = cod;
+            Descricao = desc;
+            Preco = pre;
+            Quantidade = quan;
+            Unidade = uni;
+
+        }
+    }
+}
